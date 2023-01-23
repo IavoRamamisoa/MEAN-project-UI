225603315776d9bc8012083c0166a817dbc205ee
IavoRamamisoa <ramamisoa.iavo@gmail.com>
2023-01-23 03:01:37 +0300

    Template static

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,61 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { IconsComponent } from '../../icons/icons.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+import { UpgradeComponent } from '../../upgrade/upgrade.component';
+import { ReparationComponent } from 'app/reparation/reparation.component';
+import { VoitureComponent } from 'app/voiture/voiture.component';
+
+export const AdminLayoutRoutes: Routes = [
+    // {
+    //   path: '',
+    //   children: [ {
+    //     path: 'dashboard',
+    //     component: DashboardComponent
+    // }]}, {
+    // path: '',
+    // children: [ {
+    //   path: 'userprofile',
+    //   component: UserProfileComponent
+    // }]
+    // }, {
+    //   path: '',
+    //   children: [ {
+    //     path: 'icons',
+    //     component: IconsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'notifications',
+    //         component: NotificationsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'maps',
+    //         component: MapsComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'typography',
+    //         component: TypographyComponent
+    //     }]
+    // }, {
+    //     path: '',
+    //     children: [ {
+    //         path: 'upgrade',
+    //         component: UpgradeComponent
+    //     }]
+    // }
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'voiture',   component: VoitureComponent },
+    { path: 'reparation',   component: ReparationComponent },
+    { path: 'icons',          component: IconsComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+    { path: 'upgrade',        component: UpgradeComponent },
+];
